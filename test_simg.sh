051eb7000cd5fdcb88a77451703b15ee6d671ad8
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2020-02-28 17:50:01 -0600

    test code

--- test_simg.sh
+++ test_simg.sh
@@ -4,14 +4,14 @@ singularity run \
 --cleanenv \
 --bind INPUTS:/INPUTS \
 --bind OUTPUTS:/OUTPUTS \
-baxpr-dwipre-PNC-master-v1.1.0.simg \
+baxpr-dwipre-PNC-edit-master-v1.0.0.simg \
 --dti35_niigz /INPUTS/DTI_2x32_35.nii.gz \
 --dti35_bvals /INPUTS/DTI_2x32_35.bval \
 --dti35_bvecs /INPUTS/DTI_2x32_35.bvec \
 --dti36_niigz /INPUTS/DTI_2x32_36.nii.gz \
 --dti36_bvals /INPUTS/DTI_2x32_36.bval \
 --dti36_bvecs /INPUTS/DTI_2x32_36.bvec \
---bet_opts "-f 0.3 -R" \
+--mask_niigz /INPUTS/edited_mask.nii.gz \
 --acq_params "0 -1 0 0.05" \
 --project TESTPROJ \
 --subject TESTSUBJ \
